078eb91514bbb09dd0d119871a0cb7d53f1dc152
ArpanSarkar <arpan_sarkar@g.harvard.edu>
2024-03-06 14:48:03 -0500

    updating benchmark
    Co-authored-by: ArpanSarkar <arpan_sarkar@g.harvard.edu>
    Co-authored-by: Kumaresh-Krishnan <kum2nan@gmail.com>

--- library/benchmark.py
+++ library/benchmark.py
@@ -1,73 +1,88 @@
 import subprocess
 from Bio import SeqIO
 import pandas as pd
+import pickle
 import csv
+import os
+import glob
+from tqdm import tqdm
 
 hmmfetch_command = "/n/eddy_lab/software/bin/hmmfetch"
 hmmsearch_command = "/n/eddy_lab/software/bin/hmmsearch"
 reformat_command = "/n/eddy_lab/software/bin/esl-reformat"
 align_command = "/n/eddy_lab/software/bin/hmmalign"
 alipid_command = "/n/eddy_lab/software/bin/esl-alipid"
-incE = str(1)
+incE = str(0.001) #0.001
 
-hmm_db = "/n/eddy_lab/data/pfam/pfam-35.0/Pfam-A.hmm"
-train_db = "/n/eddy_lab/Lab/protein_annotation_dl/data/train_fasta/train_ids_full.fasta"
-test_db = "/n/eddy_lab/Lab/protein_annotation_dl/data/test_fasta/test_ids_full.fasta"
+hmm_db = "/n/eddy_lab/data/pfam-35.0/Pfam-A.hmm"
+train_db = "/n/eddy_lab/Lab/protein_annotation_dl/PSALM/data/train_fasta/train_ids_full.fasta"
+test_db = "/n/eddy_lab/Lab/protein_annotation_dl/PSALM/data/test_fasta/test_ids_full.fasta"
 
 save_path = "/n/eddy_lab/Lab/protein_annotation_dl/PSALM/data/benchmarking"
-output_hmm = f"{save_path}/temp.hmm"
-output_tr_sto = f"{save_path}/temp_tr_msa.sto"
-output_te_sto = f"{save_path}/temp_te_msa.sto"
-output_tr_fasta = f"{save_path}/temp_tr.fasta"
-output_te_fasta = f"{save_path}/temp_te.fasta"
+output_hmm = f"{save_path}/tmp.hmm"
+output_tr_sto = f"{save_path}/tmp_tr_msa.sto"
+output_te_sto = f"{save_path}/tmp_te_msa.sto"
+output_tr_fasta = f"{save_path}/tmp_tr.fasta"
+output_te_fasta = f"{save_path}/tmp_te.fasta"
 tr_MMSA = f"{save_path}/train.seed.sto"
-temp_align = f"{save_path}/temp_tr_te.sto"
-temp_alipid = f"{save_path}/temp_alipid.txt"
+tmp_align = f"{save_path}/tmp_tr_te.sto"
+tmp_alipid = f"{save_path}/tmp_alipid.txt"
 
 # Open log file
 with open(f"{save_path}/log.csv", "w") as log_file:
     writer = csv.writer(log_file)
 
-# For a given family ID
-family_id = "PF10417.12"
+    # Get list of family IDs
+    with open(f"{save_path}"+"/../maps.pkl", "rb") as f:
+        maps = pickle.load(f)
+    family_ids = list(maps.keys())
 
-# hmmfetch profile for selected family
-subprocess.run([hmmfetch_command, "-o", output_hmm, hmm_db, family_id], stdout=subprocess.DEVNULL)
+    # Iterate through each family
+    for family_id in tqdm(family_ids):
+        # family_id = "PF10417.12"
 
-# hmmsearch profile against test database and return MSA of hits
-subprocess.run([hmmsearch_command, "-A", output_te_sto, "--incE", incE, output_hmm, test_db], stdout=subprocess.DEVNULL)
-'''
-****IF NO HITS ARE RETURNED, SKIP THE REST OF THE CODE****
-'''
+        # hmmfetch profile for selected family
+        subprocess.run([hmmfetch_command, "-o", output_hmm, hmm_db, family_id], stdout=subprocess.DEVNULL)
 
-# hmmsearch profile against training database and return MSA of hits
-subprocess.run([hmmsearch_command, "-A", output_tr_sto, "--incE", incE, output_hmm, train_db], stdout=subprocess.DEVNULL)
+        # hmmsearch profile against test database and return MSA of hits
+        subprocess.run([hmmsearch_command, "-A", output_te_sto, "--incE", incE, output_hmm, test_db], stdout=subprocess.DEVNULL)
 
-# reformat train and test stocholm files to fasta format
-subprocess.run([reformat_command, "--informat", "stockholm", "-o", output_tr_fasta, "fasta", output_tr_sto])
-subprocess.run([reformat_command, "--informat", "stockholm", "-o", output_te_fasta, "fasta", output_te_sto])
+        # hmmsearch profile against training database and return MSA of hits
+        subprocess.run([hmmsearch_command, "-A", output_tr_sto, "--incE", incE, output_hmm, train_db], stdout=subprocess.DEVNULL)
 
-# Take each sequence from test and gets its max pid with train
-tr_sequences = list(SeqIO.parse(output_tr_fasta, "fasta"))
-te_sequences = SeqIO.parse(output_te_fasta, "fasta")
-for seq in te_sequences:
-    # Write all of train and a single test sequence to a file
-    filename = f"{save_path}/temp_{seq.id.replace('/', '_')}.fasta"
-    with open(filename, "w+") as f:
-        SeqIO.write(tr_sequences, f, "fasta") 
-        SeqIO.write(seq, f, "fasta")
-    
-    # hmmalign the file to the corresponding profile hmm
-    subprocess.run([align_command, "-o", temp_align, output_hmm, filename])
+        # reformat train and test stocholm files to fasta format
+        subprocess.run([reformat_command, "--informat", "stockholm", "-o", output_tr_fasta, "fasta", output_tr_sto])
+        subprocess.run([reformat_command, "--informat", "stockholm", "-o", output_te_fasta, "fasta", output_te_sto])
 
-    # get alipid for the alignment
-    with open(temp_alipid, "w") as f:
-        subprocess.run([alipid_command, temp_align], stdout=f)
+        # Take each sequence from test and gets its max pid with train
+        tr_sequences = list(SeqIO.parse(output_tr_fasta, "fasta"))
+        te_sequences = SeqIO.parse(output_te_fasta, "fasta")
+        for seq in te_sequences:
+            # Write all of train and a single test sequence to a file
+            filename = f"{save_path}/tmp_{seq.id.replace('/', '_')}.fasta"
+            with open(filename, "w+") as f:
+                SeqIO.write(tr_sequences, f, "fasta") 
+                SeqIO.write(seq, f, "fasta")
+            
+            # hmmalign the file to the corresponding profile hmm
+            subprocess.run([align_command, "-o", tmp_align, output_hmm, filename])
 
-    # Retrieve the max pid from the alipid file
-    df = pd.read_csv(temp_alipid, sep="\s+", usecols=[0, 1, 2], names = ["seq1","seq2","pid"],header=0)
-    subset_df = df[(df['seq1'] == seq.id) | (df['seq2'] == seq.id)]
-    max_pid = subset_df['pid'].max()
+            # get alipid for the alignment
+            with open(tmp_alipid, "w") as f:
+                subprocess.run([alipid_command, tmp_align], stdout=f)
 
-    # Log <seq_id, domain, family_pid>
-    writer.writerow([seq.id, family_id, max_pid])
+            # Retrieve the max pid from the alipid file
+            df = pd.read_csv(tmp_alipid, sep="\s+", usecols=[0, 1, 2], names = ["seq1","seq2","pid"],header=0)
+            subset_df = df[(df['seq1'] == seq.id) | (df['seq2'] == seq.id)]
+            max_pid = subset_df['pid'].max()
+
+            # Log <seq_id, domain, family_pid>
+            writer.writerow([seq.id, family_id, max_pid])
+
+        # Remove tmp files from save_path
+        tmp_files = glob.glob(f"{save_path}/tmp*")
+        for file in tmp_files:
+            try:
+                os.remove(file)
+            except Exception as e:
+                print(f"Error deleting file {file}: {e}")
